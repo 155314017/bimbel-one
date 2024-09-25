ef886d703cf869dccd232f7dd863e908283931aa
Dito <dhito.pb97@gmail.com>
2024-09-25 08:34:36 +0700

    show data in student table

--- src/pages-cms/Admin/Student/StudentTable.tsx
+++ src/pages-cms/Admin/Student/StudentTable.tsx
@@ -2,90 +2,140 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import SearchBar from "../../../components/small/SearchBar";
-import DataRegis from "../../../dummyData/regisData";
-import DetailStudent from "./DetailStudent";
+import { Toaster, toast } from "react-hot-toast";
+import Spinners from "../../../assets/spinners.svg";
+import { useDataStudent } from "../../../services/API/cms/FetchDataStudent";
 
 interface Student {
   id: string;
-  name: string;
+  teacher_id: string | null;
+  full_name: string;
+  email: string;
+  type: string;
   status: string;
 }
 
 export default function StudentTable() {
-  const datas = DataRegis;
+  const [page, setPage] = useState<number>(1);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [dataPerPage] = useState(10);
-  const [selectedStudent, setSelectedStudent] = useState<Student | null>(null);
+  const {
+    data: apiResponse = { data: { user: [] } },
+    isLoading,
+    isError,
+  } = useDataStudent();
 
-  const indexOfLastData = currentPage * dataPerPage;
-  const indexOfFirstData = indexOfLastData - dataPerPage;
-  const currentData = datas.slice(indexOfFirstData, indexOfLastData);
+  const dataStudent = apiResponse.data.user || [];
 
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const totalPages = Math.ceil(dataStudent.length / rowsPerPage);
 
-  const handleStudentDetail = (studentId: string, studentName: string, studentStatus: string) => {
-    setSelectedStudent({ id: studentId, name: studentName, status: studentStatus });
-  };
-
-  if (selectedStudent) {
+  if (isLoading) {
     return (
-      <DetailStudent
-        student={selectedStudent}
-        onBack={() => setSelectedStudent(null)}
-      />
+      <div className="flex justify-center items-center mt-3">
+        <img src={Spinners} alt="spinner" className="w-10" />
+      </div>
     );
   }
 
+  if (isError) {
+    toast.error("Failed to fetch data");
+    return (
+      <div className="font-bold text-center text-lg mt-3">
+        Error loading data
+      </div>
+    );
+  }
+
+  const displayData = dataStudent.slice(
+    (page - 1) * rowsPerPage,
+    page * rowsPerPage
+  );
+
+  const handleChangePage = (newPage: number) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setPage(newPage);
+    }
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(1);
+  };
+
+  const paginationNumbers = () => {
+    const pageRange = [];
+    const maxButtons = 5;
+
+    if (totalPages <= maxButtons) {
+      for (let i = 1; i <= totalPages; i++) {
+        pageRange.push(i);
+      }
+    } else {
+      if (page <= 3) {
+        pageRange.push(1, 2, 3, 4, "...", totalPages);
+      } else if (page > totalPages - 3) {
+        pageRange.push(
+          1,
+          "...",
+          totalPages - 3,
+          totalPages - 2,
+          totalPages - 1,
+          totalPages
+        );
+      } else {
+        pageRange.push(1, "...", page - 1, page, page + 1, "...", totalPages);
+      }
+    }
+    return pageRange;
+  };
+
   return (
     <div>
+      <Toaster />
       <div className="mb-5 flex justify-between items-center">
         <h1 className="text-xl font-bold">Student List</h1>
         <SearchBar />
       </div>
-
       <div className="max-h-[500px] overflow-y-auto">
         <table className="table-auto w-full border">
           <thead className="border">
             <tr className="sticky top-0 bg-slate-50">
-              <th className="border text-[12px]  p-1">Id</th>
-              <th className="border text-[12px]  p-1">Name</th>
-              <th className="border text-[12px]  p-1">Email</th>
-              <th className="border text-[12px]  p-1">Phone Number</th>
-              <th className="border text-[12px]  p-1">Status</th>
+              <th className="border text-[12px] p-1">Id</th>
+              <th className="border text-[12px] p-1">Name</th>
+              <th className="border text-[12px] p-1">Email</th>
+              <th className="border text-[12px] p-1">Type</th>
+              <th className="border text-[12px] p-1">Status</th>
               <th className="border text-[12px] p-1 w-[100px]">Detail</th>
             </tr>
           </thead>
           <tbody>
-            {currentData.map((data, index) => (
-              <tr key={index}>
+            {displayData.map((student: Student) => (
+              <tr key={student.id}>
                 <td className="border capitalize p-2 text-center text-[12px]">
-                  {data.id}
+                  {student.teacher_id || "-"}
                 </td>
                 <td className="border capitalize p-2 text-[12px] bg-slate-100">
-                  {data.name}
+                  {student.full_name}
                 </td>
-                <td className="border p-2 text-[12px]">{data.email}</td>
+                <td className="border p-2 text-[12px]">{student.email}</td>
                 <td className="border capitalize p-2 text-center text-[12px] bg-slate-100">
-                  {data.phoneNumber}
+                  {student.type}
                 </td>
                 <td className="border p-2 text-center text-[12px]">
                   <span
-                    className={`px-2 py-1 rounded-md font-bold tracking-wide 
-      ${
-        data.status === "active"
-          ? "bg-green-100 text-green-500"
-          : "bg-red-100 text-red-500"
-      }`}
+                    className={`px-2 py-1 rounded-md font-bold tracking-wide ${
+                      student.status === "active"
+                        ? "bg-green-100 text-green-500"
+                        : "bg-red-100 text-red-500"
+                    }`}
                   >
-                    {data.status}
+                    {student.status}
                   </span>
                 </td>
                 <td className="border capitalize p-2 flex justify-center gap-5 bg-slate-100">
-                  <button
-                    onClick={() => handleStudentDetail(data.id, data.name, data.status)}
-                    className="border-2 border-slate-300 w-[25px] h-[25px] rounded-md flex justify-center bg-white items-center text-[12px] transition-all ease hover:border-[#125B9A]"
-                  >
+                  <button className="border-2 border-slate-300 w-[25px] h-[25px] rounded-md flex justify-center bg-white items-center text-[12px] transition-all ease hover:border-[#125B9A]">
                     <FontAwesomeIcon
                       className="text-[#125B9A]"
                       icon={faEllipsisVertical}
@@ -97,121 +147,64 @@ export default function StudentTable() {
           </tbody>
         </table>
       </div>
-      {/* Pagination */}
-      <Pagination
-        key={datas.length}
-        dataPerPage={dataPerPage}
-        totalData={datas.length}
-        paginate={paginate}
-        currentPage={currentPage}
-      />
+
+      {/* Pagination Controls */}
+      <div className="flex justify-between items-center mt-4 gap-3">
+        <h1 className="text-[14px] text-slate-400">
+          Showing {displayData.length} of {dataStudent.length}
+        </h1>
+
+        <div className="flex gap-5">
+          <div className="flex gap-3 items-center text-[14px] text-slate-400">
+            <select value={rowsPerPage} onChange={handleChangeRowsPerPage}>
+              <option value={10}>10 rows</option>
+              <option value={25}>25 rows</option>
+              <option value={50}>50 rows</option>
+            </select>
+          </div>
+
+          <div className="flex gap-3">
+            {/* Previous button */}
+            <button
+              className="shadow-sm border-2 rounded-md capitalize bg-[#125B9A] text-slate-50 text-[12px] px-2 py-1 tracking-wide transition-all ease hover:border-[#125B9A] hover:text-[#125B9A] hover:bg-slate-50"
+              onClick={() => handleChangePage(page - 1)}
+              disabled={page === 1}
+            >
+              Previous
+            </button>
+
+            {/* Pagination Numbers */}
+            <div className="flex gap-2">
+              {paginationNumbers().map((number, index) =>
+                typeof number === "number" ? (
+                  <button
+                    key={index}
+                    className={`shadow-sm border-2 rounded-md capitalize bg-slate-300 text-[#125B9A] text-[12px] w-[30px] py-1 tracking-wide transition-all ease hover:border-[#125B9A] hover:text-[#125B9A] hover:bg-slate-50 ${
+                      page === number ? "bg-slate-50 text-[#125B9A]" : ""
+                    }`}
+                    onClick={() => handleChangePage(number)}
+                  >
+                    {number}
+                  </button>
+                ) : (
+                  <span key={index} className="text-slate-500">
+                    ...
+                  </span>
+                )
+              )}
+            </div>
+
+            {/* Next button */}
+            <button
+              className="shadow-sm border-2 rounded-md capitalize bg-[#125B9A] text-slate-50 text-[12px] px-2 py-1 tracking-wide transition-all ease hover:border-[#125B9A] hover:text-[#125B9A] hover:bg-slate-50"
+              onClick={() => handleChangePage(page + 1)}
+              disabled={page === totalPages}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-interface PaginationProps {
-  dataPerPage: number;
-  totalData: number;
-  paginate: (pageNumber: number) => void;
-  currentPage: number;
-}
-
-const Pagination = ({
-  dataPerPage,
-  totalData,
-  paginate,
-  currentPage,
-}: PaginationProps) => {
-  // const pageNumbers = [];
-  const totalPages = Math.ceil(totalData / dataPerPage);
-  const siblingsCount = 1;
-
-  const showEllipsis = () => {
-    return <li className="px-2 py-1">...</li>;
-  };
-
-  const generatePageNumbers = () => {
-    const pages = [];
-
-    if (currentPage > siblingsCount + 2) {
-      pages.push(1, showEllipsis());
-    } else {
-      for (let i = 1; i < Math.min(siblingsCount + 1, totalPages); i++) {
-        pages.push(i);
-      }
-    }
-
-    const startPage = Math.max(2, currentPage - siblingsCount);
-    const endPage = Math.min(totalPages - 1, currentPage + siblingsCount);
-
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(i);
-    }
-
-    if (currentPage < totalPages - siblingsCount - 1) {
-      pages.push(showEllipsis(), totalPages);
-    }
-
-    return pages;
-  };
-
-  const pages = generatePageNumbers();
-
-  return (
-    <nav className="mt-4 flex justify-between items-center">
-      <h1 className="text-[14px] text-slate-400">
-        Showing {currentPage * dataPerPage - dataPerPage + 1} to{" "}
-        {currentPage * dataPerPage} of {totalData}
-      </h1>
-      <ul className="flex justify-center gap-2">
-        {/* Tombol halaman sebelumnya */}
-        <li>
-          <button
-            onClick={() => paginate(currentPage - 1)}
-            disabled={currentPage === 1}
-            className={`px-4 py-2 rounded-md text-[12px] ${
-              currentPage === 1 ? "bg-gray-300" : "bg-blue-500 text-white"
-            }`}
-          >
-            Previous
-          </button>
-        </li>
-
-        {/* Nomor halaman */}
-        {pages.map((page, index) =>
-          typeof page === "number" ? (
-            <li key={index}>
-              <button
-                onClick={() => paginate(page)}
-                className={`px-4 py-2 rounded-md text-[12px] ${
-                  page === currentPage
-                    ? "bg-blue-500 text-white"
-                    : "bg-gray-200 text-gray-700"
-                }`}
-              >
-                {page}
-              </button>
-            </li>
-          ) : (
-            page
-          )
-        )}
-
-        {/* Tombol halaman berikutnya */}
-        <li>
-          <button
-            onClick={() => paginate(currentPage + 1)}
-            disabled={currentPage === totalPages}
-            className={`px-4 py-2 rounded-md text-[12px] ${
-              currentPage === totalPages
-                ? "bg-gray-300"
-                : "bg-blue-500 text-white"
-            }`}
-          >
-            Next
-          </button>
-        </li>
-      </ul>
-    </nav>
-  );
-};
